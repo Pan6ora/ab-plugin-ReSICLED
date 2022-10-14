9480359b87a6f605a72383daf4f4d1f5f1e74a69
Rémy Le Calloch <remy@lecalloch.net>
2022-10-14 16:02:50 +0200

    Move db tab to left panel and delete panel.py

--- plugin/layouts/tabs/left.py
+++ plugin/layouts/tabs/left.py
@@ -1,12 +1,27 @@
 from PySide2 import QtCore, QtWidgets
+from PySide2.QtWidgets import (
+    QCheckBox, QFileDialog, QHBoxLayout, QMessageBox, QPushButton, QToolBar,
+    QStyle, QVBoxLayout, QTabWidget, QComboBox
+)
 from activity_browser.layouts.tabs import PluginTab
 from activity_browser.ui.style import horizontal_line, header
+
+from ...signals import signals
+from ...views.databaseTab import DatabaseTab
 
 class LeftTab(PluginTab):
     def __init__(self,plugin,parent=None):
         super(LeftTab, self).__init__(plugin=plugin,panel="left", parent=parent)
-        self.layout = QtWidgets.QVBoxLayout()
+        self.tabs = dict([('index1',1),('index2',2)])
+        self.tabwidget = QTabWidget()
+        self.tabs = {
+            "Database": DatabaseTab(self),
+		}
+        for tab_name, tab_obj in self.tabs.items():
+            self.tabwidget.addTab(tab_obj, tab_name)
+            
+        #display tab in layout   
+        self.layout = QVBoxLayout()
         self.layout.setAlignment(QtCore.Qt.AlignTop)
-        self.layout.addWidget(header("ReSICLED"))
-        self.layout.addWidget(horizontal_line())
+        self.layout.addWidget(self.tabwidget)
         self.setLayout(self.layout)
--- plugin/layouts/tabs/right.py
+++ plugin/layouts/tabs/right.py
@@ -1,16 +1,43 @@
 from PySide2 import QtCore, QtWidgets
+from PySide2.QtWidgets import (
+    QCheckBox, QFileDialog, QHBoxLayout, QMessageBox, QPushButton, QToolBar,
+    QStyle, QVBoxLayout, QTabWidget, QComboBox
+)
 from activity_browser.layouts.tabs import PluginTab
 from activity_browser.ui.style import horizontal_line, header
-from ...views.panel import ResicledTab
+
+from ...signals import signals
+from ...views.PresentationTab import PresentationTab
+from ...views.inputTab import InputTab
+from ...views.dismantlingTab import DismantlingTab
+from ...views.shreddingTab import ShreddingTab
+from ...views.mixedTab import MixedTab
+from ...views.hotspotsTab import HotspotsTab
+from ...views.guidelinesTab import GuidelinesTab
 
 class RightTab(PluginTab):
     def __init__(self, plugin, parent=None):
         super(RightTab, self).__init__(plugin=plugin, panel="right", parent=parent)
-        self.layout = QtWidgets.QVBoxLayout()
+        self.tabs = dict([('index1',1),('index2',2)])
+        self.tabwidget = QTabWidget()
+        self.tabs = {
+            "Presentation": PresentationTab(self),
+            "Input": InputTab(self),
+            "Dismantling": DismantlingTab(self),
+            "Shredding": ShreddingTab(self),
+            "Mixed": MixedTab(self),
+            "HotSpots": HotspotsTab(self),
+            "Guidelines": GuidelinesTab(self),
+		}
+        for tab_name, tab_obj in self.tabs.items():
+            self.tabwidget.addTab(tab_obj, tab_name)
+            if(tab_name=="Dismantling" or tab_name=="Shredding" or tab_name=="Mixed" or tab_name=="HotSpots"):
+                signals.update_combobox.connect(tab_obj.update_menu_combobox)
+                signals.update_combobox.emit(QComboBox())
+            
+        #display tab in layout   
+        self.layout = QVBoxLayout()
         self.layout.setAlignment(QtCore.Qt.AlignTop)
-        self.layout.addWidget(header("ReSICLED"))
-        self.layout.addWidget(horizontal_line())
-        resicledtab = ResicledTab()
-        self.layout.addWidget(resicledtab)
+        self.layout.addWidget(self.tabwidget)
         self.setLayout(self.layout)
 
