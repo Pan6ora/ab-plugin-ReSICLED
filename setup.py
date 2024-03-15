779ee845a3d48d4fad7318548df8ef5540cd4a09
Rémy Le Calloch <remy@lecalloch.net>
2024-03-15 14:35:19 +0100

    Fix: package data paths

--- setup.py
+++ setup.py
@@ -10,17 +10,18 @@ accepted_filetypes = (".html", ".png", ".svg", ".js", ".css")
 
 for dirpath, dirnames, filenames in os.walk("ab_plugin_resicled"):
     # Ignore dirnames that start with '.'
-    if ('__init__.py' in filenames
-            or any(x.endswith(accepted_filetypes) for x in filenames)):
-        pkg = dirpath.replace(os.path.sep, '.')
+    if "__init__.py" in filenames or any(
+        x.endswith(accepted_filetypes) for x in filenames
+    ):
+        pkg = dirpath.replace(os.path.sep, ".")
         if os.path.altsep:
-            pkg = pkg.replace(os.path.altsep, '.')
+            pkg = pkg.replace(os.path.altsep, ".")
         packages.append(pkg)
 
-if 'VERSION' in os.environ:
-    version = os.environ['VERSION']
+if "VERSION" in os.environ:
+    version = os.environ["VERSION"]
 else:
-    version = os.environ.get('GIT_DESCRIBE_TAG', '0.0.0')
+    version = os.environ.get("GIT_DESCRIBE_TAG", "0.0.0")
 
 setup(
     name="ab-plugin-resicled",
@@ -29,14 +30,16 @@ setup(
     include_package_data=True,
     author="G-SCOP",
     author_email="remy@lecalloch.net",
-    license=open('LICENSE.txt').read(),
-    install_requires=[], # dependency management in conda recipe
+    license=open("LICENSE.txt").read(),
+    install_requires=[],  # dependency management in conda recipe
     url="https://gricad-gitlab.univ-grenoble-alpes.fr/green-scop/ab-plugins/resicled",
-    long_description=open('README.md').read(),
+    long_description=open("README.md").read(),
     description="Evaluate the recyclability of product Electr(on)ic for improving product design",
-    package_data={'': [
-        'includes/resicled_directive/bw2packages/resicled_directives.bw2package', 
-        'includes/resicled_directive/bw2packages/resicled_guidelines.bw2package', 
-        'includes/resicled_directive/bw2packages/resicled_materials.bw2package', 
-        ]},
-    )
+    package_data={
+        "": [
+            "includes/bw2packages/resicled_directives.bw2package",
+            "includes/bw2packages/resicled_guidelines.bw2package",
+            "includes/bw2packages/resicled_materials.bw2package",
+        ]
+    },
+)
